8496540723ae87e1ddad779206afd958051802fd
raveeshareshan <126398788+raveeshareshan@users.noreply.github.com>
2024-01-02 22:54:57 +0530

    Tab size responsive breakpoint added

--- Ecommerce-Website/script.js
+++ Ecommerce-Website/script.js
@@ -13,3 +13,22 @@ function displayImage(){
     images[index - 1].style.display = "block";
     setTimeout(displayImage, 2000);
 }
+
+
+
+/* Hamburger menu */
+const bar = document.getElementById("mobile");
+const hamburgerMenu = document.getElementById("navbar");
+const hide = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        hamburgerMenu.classList.add('active');
+    })
+}
+
+if(hide){
+    hide.addEventListener('click', () => {
+        hamburgerMenu.classList.remove('active');
+    })
+}
